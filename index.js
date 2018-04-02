5128ef6dbbe1cd48d147745d2663ba08eab383ff
bafab <bafa@tutanota.de>
2018-04-02 11:43:00 -0400

    Done.

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ function printBadges (names) {
 }
 
 function tailsNeverFails () {
-    let tails = 0;
-    while (Math.random() >= 0.5) {
-      tails++;
-    }
-    return `You got ${tails} tails in a row!`;
+  let tails = 0;
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+  return `You got ${tails} tails in a row!`;
 }
