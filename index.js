836dfa34fe641e2b0a5a77761798388ba8be376b
alec-horwitz <horwitz_alec@wheatoncollege.edu>
2018-04-02 13:35:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+function printBadges(arry) {
+  for (var i = 0; i < arry.length; i++) {
+    console.log("Welcome "+arry[i]+"! You are employee #"+(i+1)+".")
+  }
+  return arry
+}
+
+
+function tailsNeverFails() {
+  var x = 0
+  while (maybeTrue()) {
+    x++
+  }
+  return "You got "+x+" tails in a row!"
+}
+
+function maybeTrue() {
+  x = (Math.floor(Math.random() * 2) == 0);
+  if(x){
+  	return false;
+  }else{
+    return true;
+  }
+}
