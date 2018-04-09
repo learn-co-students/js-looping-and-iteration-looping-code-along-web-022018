90077411730b0917ab599015dccd6d1e7b10e8bc
Donnovan Lyons <dlyons8@binghamton.edu>
2018-04-09 14:24:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function printBadges(array)  {
+  for (i=0; i < array.length; i++) {
+  console.log(`Welcome ${array[i]}! You are employee #${i+1}.`)
+  }
+  return array
+}
+
+function tailsNeverFails() {
+  let tails = 0
+  let heads = 0
+  while (heads === 0) {
+     if (Math.random() >= 0.5) {
+      tails += 1
+    } else {
+      heads += 1
+    }
+  }
+  return `You got ${tails} tails in a row!`
+}
